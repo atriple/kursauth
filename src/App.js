d5533f6b35d6c3d00b961418bece39c02cfff2d0
atriple <40452321+atriple@users.noreply.github.com>
2021-06-18 22:36:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,8 +30,13 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn Chakra
+              Aplikasi Kurs
             </Link>
+            <Select placeholder="Select option">
+  <option value="option1">Option 1</option>
+  <option value="option2">Option 2</option>
+  <option value="option3">Option 3</option>
+</Select>
           </VStack>
         </Grid>
       </Box>
